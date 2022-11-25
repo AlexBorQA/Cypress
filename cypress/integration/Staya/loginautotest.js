9b4ccc075e7a87f447a1c9cc4546c6d12a7746a0
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-11-25 18:02:17 +0500

    Add files via upload
    Базовая закачка без Node

---
+++ cypress/integration/Staya/loginautotest.js
@@ -0,0 +1,28 @@
+
+describe('Проверка формы логина     ', function () {
+ 
+it('Позитивный кейс: верный логин, верный пароль', function () {
+        cy.visit('https://staya.dog');
+        cy.get('.header-bottom__right--link').click();
+        cy.get('.auth-form > form > [type="email"]').type('s_ivanov2018@mail.ru');
+        cy.get('.auth-form > form > [type="password"]').type('Rgb@528651');
+        cy.get('.auth-form__submit > .s-button__content').click();
+        cy.contains('Мои заказы');
+})
+
+it('Негативный кейс: верный логин, неверный пароль', function () {
+        cy.get('button.profile__nav-link').click();
+        cy.get('.box__button_ok > .s-button__content').click();
+cy.get('.header-bottom__right--link').click();
+cy.get('.auth-form > form > [type="email"]').type('s_ivanov2018@mail.ru');
+        cy.get('.auth-form > form > [type="password"]').type('Rgb@528659');
+        cy.get('.auth-form__submit > .s-button__content').click();
+cy.contains('Невозможно войти с предоставленными учетными данными.');
+
+})
+
+
+
+
+})
+ 
