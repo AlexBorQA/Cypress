c5f05042093ae74497e824fc69d1de39ca1c22e4
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-11-25 21:22:36 +0500

    Update loginautotest.js
    Корр

--- cypress/integration/Staya/loginautotest.js
+++ cypress/integration/Staya/loginautotest.js
@@ -17,11 +17,11 @@ it('Позитивный кейс: верный логин, верный пар�
 it('Негативный кейс: верный логин, неверный пароль', function () {
         cy.get('button.profile__nav-link').click();
         cy.get('.box__button_ok > .s-button__content').click();
-cy.get('.header-bottom__right--link').click();
-cy.get('.auth-form > form > [type="email"]').type('s_ivanov2018@mail.ru');
+        cy.get('.header-bottom__right--link').click();
+        cy.get('.auth-form > form > [type="email"]').type('s_ivanov2018@mail.ru');
         cy.get('.auth-form > form > [type="password"]').type('Rgb@528659');
         cy.get('.auth-form__submit > .s-button__content').click();
-cy.contains('Невозможно войти с предоставленными учетными данными.');
+        cy.contains('Невозможно войти с предоставленными учетными данными.');
 
 })
 
